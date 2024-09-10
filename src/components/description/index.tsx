9440c884bda3409d0caf32ec233a0bd601832e45
israelcruzz <israelcruz.contato@gmail.com>
2024-09-10 10:38:00 -0300

    feat: add description component

--- src/components/description/index.tsx
+++ src/components/description/index.tsx
@@ -1,5 +1,16 @@
-export const Description = () => {
-    return (
-        <p></p>
-    )
+interface DescriptionProps {
+  text: string;
+  variant: "primary" | "secondary";
 }
+
+export const Description = ({ text, variant }: DescriptionProps) => {
+  return (
+    <p
+      className={`font-medium text-lg text-[${
+        variant === "primary" ? "#7F7F7F" : "#FFFFFF"
+      }]`}
+    >
+      {text}
+    </p>
+  );
+};
