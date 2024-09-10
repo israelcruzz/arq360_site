66cb28448a0d80b2dfbb73542f1e5d1811039df3
israelcruzz <israelcruz.contato@gmail.com>
2024-09-10 10:34:17 -0300

    refactor: add title in heading component props

---
+++ src/components/description/index.tsx
@@ -0,0 +1,5 @@
+export const Description = () => {
+    return (
+        <p></p>
+    )
+}
--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -1,11 +1,16 @@
 interface HeadingProps {
   variant: "xs" | "lg";
+  title: string;
 }
 
-export const Heading = ({ variant }: HeadingProps) => {
+export const Heading = ({ variant, title }: HeadingProps) => {
   return (
     <h1
-      className={`text-black font-bold text-[${variant === "xs" ? "64px" : "48px"}]`}
-    ></h1>
+      className={`text-black font-bold text-[${
+        variant === "xs" ? "64px" : "48px"
+      }]`}
+    >
+      {title}
+    </h1>
   );
 };
