0d29ae2c2e0cf6a7ef4526b7c8034eb634d389dc
israelcruzz <israelcruz.contato@gmail.com>
2024-09-10 10:58:46 -0300

    feat: add button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,33 @@
+import React, { HTMLAttributes } from "react";
+import { twMerge } from "tailwind-merge";
+
+type ButtonVariantTypeProps = "primary" | "secondary";
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant: ButtonVariantTypeProps;
+}
+
+const ButtonVariantsRecord: Record<ButtonVariantTypeProps, string> = {
+  primary: "bg-black text-white hover:bg-black/90",
+  secondary: "bg-white text-black hover:bg-white/90",
+};
+
+export const Button = ({
+  children,
+  variant,
+  className,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      className={twMerge(
+        `py-4 px-11 rounded-lg flex justify-center items-center font-medium text-lg ${ButtonVariantsRecord[variant]}`,
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import { Button } from "../components/button";
+
 function App() {
     return (
       <>
         <h1 className="bg-red-500">ola</h1>
+        <Button variant="primary">
+          <h1>Download</h1>
+        </Button>
       </>
     )
   }
